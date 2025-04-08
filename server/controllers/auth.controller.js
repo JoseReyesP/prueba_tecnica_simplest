8ed0c603fa3d50e3676b4004cdd6c56f37ea96c9
JoseReyesP <relujo@hotmail.com>
2025-04-08 14:39:26 -0500

    create user model, auth controler, and user controller

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,75 +1,57 @@
 const User = require("../models/user.model.js");
+const userCtrl = require("../controllers/user.controller.js");
+const jwt = require("jsonwebtoken");
+const { expressjwt } = require("express-jwt");
+const config = require("../../config/config.js");
 
-const create = async (req, res) => {
-  const user = new User(req.body);
+const signin = async (req, res) => {
+  const { email, password } = req.body;
   try {
-    await user.save();
-    return res.status(200).json({ message: "Successfully signed up!" });
-  } catch (error) {
-    return res
-      .status(400)
-      .json({ error: "Error while creating new user: " + err });
-  }
-};
-
-const list = async (req, res) => {
-  try {
-    let users = await User.find().select("name lastname email");
-    res.status(200).json(users);
-  } catch (error) {
-    res.status(400).json({ error: error.message });
-  }
-};
-
-const userByID = async (req, res, next, id) => {
-  try {
-    let user = await User.findById(id);
-    if (!user) {
-      return res.status(400).json({
-        error: "User by Id: User not found",
-      });
+    let user = await User.findOne({ email: email });
+    if (!user) return res.status(401).json({ error: "User not found" });
+    if (!user.authenticate(password)) {
+      return res.status(401).send({ error: "Email and password don't match." });
     }
-    req.profile = user;
-    next();
-  } catch (error) {
-    return res.status(400).json({
-      error: "Could not retreive user",
+    const token = jwt.sign({ _id: user._id }, config.jwtSecret);
+    res.cookie("t", token, { expire: new Date() + 9999 });
+
+    return res.json({
+      token,
+      user: {
+        _id: user._id,
+        name: user.name,
+        lastname: user.lastname,
+        email: user.email,
+      },
     });
-  }
-};
-
-const read = (req, res) => {
-  try {
-    const decyptedPassword = crypto
-      .createHmac("sha1", req.profile.hashed_password)
-      .update(req.profile.salt)
-      .digest("hex");
-    req.profile.decryptedPassword = decyptedPassword;
   } catch (error) {
-    console.log(error);
-  }
-
-  return res.json(req.profile);
-};
-
-const update = async (req, res) => {
-  try {
-    let user = req.profile;
-    req.body = { ...req.body, updated: Date.now() };
-    await User.findByIdAndUpdate(user._id, { $set: req.body }, { new: true });
-    res
-      .status(200)
-      .json({ message: "The profile has been updated successfully" });
-  } catch (error) {
-    return res.status(400).json({
-      error: err.message,
-    });
+    return res.status(401).json({ error: "Could not sign in" });
   }
 };
 
-const remove = async (req, res) => {
-  try {
-    let user = req.profile;
-    // there are 2 types of deletion, soft and hard, for now let's use hard deletion
-  } catch (error) {}
+const signout = (req, res) => {
+  res.clearCookie("t");
+  return res.status(200).json({ message: "signed out" });
+};
+
+const requireSignin = expressjwt({
+  secret: config.jwtSecret,
+  userProperty: "auth",
+  algorithms: ["HS256"],
+});
+
+const hasAuthorization = async (req, res, next) => {
+  const authorized =
+    req.profile && req.auth && req.profile._id.toString() == req.auth._id;
+  if (!authorized) {
+    return res.status(403).json({ error: "User is not authorized" });
+  }
+  next();
+};
+
+export default {
+  signin,
+  signout,
+  requireSignin,
+  hasAuthorization,
 };
---
+++ server/controllers/user.controller.js
@@ -0,0 +1,91 @@
+const User = require("../models/user.model.js");
+const crypto = require("crypto");
+
+const create = async (req, res) => {
+  const user = new User(req.body);
+  try {
+    await user.save();
+    return res.status(200).json({ message: "Successfully signed up!" });
+  } catch (error) {
+    return res
+      .status(400)
+      .json({ error: "Error while creating new user: " + err });
+  }
+};
+
+const list = async (req, res) => {
+  try {
+    let users = await User.find().select("name lastname email");
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+const userByID = async (req, res, next, id) => {
+  try {
+    let user = await User.findById(id);
+    if (!user) {
+      return res.status(400).json({
+        error: "User by Id: User not found",
+      });
+    }
+    req.profile = user;
+    next();
+  } catch (error) {
+    return res.status(400).json({
+      error: "Could not retreive user",
+    });
+  }
+};
+
+const read = (req, res) => {
+  try {
+    const decyptedPassword = crypto
+      .createHmac("sha1", req.profile.hashed_password)
+      .update(req.profile.salt)
+      .digest("hex");
+    req.profile.decryptedPassword = decyptedPassword;
+  } catch (error) {
+    console.log(error);
+  }
+
+  return res.json(req.profile);
+};
+
+const update = async (req, res) => {
+  try {
+    let user = req.profile;
+    req.body = { ...req.body, updated: Date.now() };
+    await User.findByIdAndUpdate(user._id, { $set: req.body }, { new: true });
+    res
+      .status(200)
+      .json({ message: "The profile has been updated successfully" });
+  } catch (error) {
+    return res.status(400).json({
+      error: err.message,
+    });
+  }
+};
+
+const remove = async (req, res) => {
+  try {
+    let user = req.profile;
+    // there are 2 types of deletion, soft and hard, for now let's use hard deletion
+    await User.findByIdAndDelete(user._id);
+    res.status(200).json({ message: `User ${user._id} has been deleted!` });
+  } catch (error) {
+    return res.status(400).json({
+      error: err.message,
+    });
+  }
+};
+
+export default {
+  create,
+  userByID,
+  read,
+  list,
+  remove,
+  update,
+};
